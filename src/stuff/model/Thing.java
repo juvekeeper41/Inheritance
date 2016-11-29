68a6e17ead6832c36171c8c2e735a37501db7d6c
juvekeeper41 <juvekeeper41@gmail.com>
2016-11-29 08:43:12 -0700

    Created inheritance project.

---
+++ src/stuff/model/Thing.java
@@ -0,0 +1,16 @@
+package stuff.model;
+
+public abstract class Thing
+{
+	private String thingData;
+
+	public String getTHingData()
+	{
+		return thingData;
+	}
+
+	public void setThingData(String data)
+	{
+		this.thingData = data;
+	}
+}
